b9a6004ad9d474dbfa5e6ba6d01069f74a16e6eb
Morris <I523193@brian.morris01@sap.com>
2025-02-06 13:15:34 -0800

    fleshing out pipelien steps

--- cloud/pipeline.py
+++ cloud/pipeline.py
@@ -19,28 +19,54 @@ pipeline_name = 'stock-pipeline'
     instance_type="ml.m5.large"
 )
 def fetch():
-    # fetch data
-    ticker = "SPY"
+    # for default ticker (SPY), 
+    # fetch latest data from yfinance
+    # Save to default_bucket
     logging.info('executing fetch step')
-    return ticker
+    return "stub_fetch"
 
 
 @step(
     instance_type="ml.m5.large"
 )
 def preprocess(data):
-    # fetch data
-    ticker = "SPY"
+    # retrieve latest data csv
+    # remove uncessary columns, format rows, etc
+    # save to bucket with postfix _preprocessed
     logging.info(f'data should be output of fetch function: {data}')
-    return ticker
+    return "stub_preprocess"
+
+@step(
+    instance_type="ml.m5.large"
+)
+def train_and_eval(data):
+    # retrieve preprocessed csv data
+    # train model
+    # load current model performance
+    # if model is better than current
+    # save model to s3 bucket /model with postfix of date
+    # -- combining training and eval to prevent unecessary model saving  
+    logging.info(f'data should be output of preprocess function: {data}')
+    return "stub_train"
+
+@step(
+    instance_type="ml.m5.large"
+)
+def deploy(data):
+    # if latest_model is different than current
+    # update deployment to use latest model
+    logging.info(f'data should be output of train function: {data}')
+    return "stub_eval"
 
 
 fetch_result = fetch()
 preprocess_result = preprocess(fetch_result)
+train_and_eval_result = train_and_eval(preprocess_result)
+deploy_result = deploy(train_and_eval_result)
 
 pipeline = Pipeline(
     name=pipeline_name,
-    steps=[fetch_result, preprocess_result],
+    steps=[fetch_result, preprocess_result, train_and_eval_result, deploy_result],
     sagemaker_session=session,
 )
 
